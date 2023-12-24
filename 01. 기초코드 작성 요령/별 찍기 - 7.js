bd78c342f324674f9d263313d5c3572ac145cfa2
Jung Wook Park <hatchling3713@gmail.com>
2023-12-25 01:38:54 +0900

    [ PS ] : 2444

---
+++ 01. 기초코드 작성 요령/별 찍기 - 7.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+const input = Number.parseInt(fs.readFileSync('/dev/stdin').toString().trim(), 10);
+
+let result = [];
+
+for (let i = 0; i < input; i++) {
+  let line = [];
+
+  for (let j = input - i - 1; j > 0; j--) {
+    line.push(' ');
+  }
+
+  for (let k = 0; k < i * 2 + 1; k++) {
+    line.push('*');
+  }
+
+  result.push(line.join(''));
+}
+
+for (let i = 1; i < input; i++) {
+  let line = [];
+
+  for (let j = 0; j < i; j++) {
+    line.push(' ');
+  }
+
+  for (let k = (input - i) * 2 - 1; k > 0; k--) {
+    line.push('*');
+  }
+
+  result.push(line.join(''));
+}
+
+console.log(result.join('\n'));
