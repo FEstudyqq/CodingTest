eb42e08b634bcd44f192af192782bc1430c08ce8
uraflower <82873315+uraflower@users.noreply.github.com>
2024-01-03 17:46:24 +0900

    [ PS ] : 9536

---
+++ 02. 문자열/여우는 어떻게 울지.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const T = Number(input.shift());
+
+for (let i = 0; i < T; i++) {
+  const endIndex = input.findIndex(
+    (string) => string === 'what does the fox say?',
+  );
+  const testCase = input.splice(0, endIndex + 1);
+  testCase.pop(); // 'what does fox say?' 원소 제거
+
+  const recordedSounds = testCase.shift().split(' ');
+  const otherSays = new Set(testCase.map((sound) => sound.split(' ')[2]));
+  const foxSays = recordedSounds.filter((sound) => !otherSays.has(sound));
+  console.log(...foxSays);
+}
