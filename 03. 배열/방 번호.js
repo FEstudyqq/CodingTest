de0c3e06868506aa8beeccc2979685eaca914c2b
uraflower <82873315+uraflower@users.noreply.github.com>
2024-01-04 16:31:35 +0900

    [ PS ] : 1475

---
+++ 03. 배열/방 번호.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const numbers = [...fs.readFileSync('/dev/stdin').toString().trim()].map(
+  Number,
+);
+
+solution();
+
+function solution() {
+  let cnt = Array.from({ length: 10 }).fill(0);
+  numbers.forEach((num) => cnt[num]++);
+
+  // 6, 9 뒤집어서 이용 가능하면 뒤집기
+  cnt[6] = Math.round((cnt[9] + cnt[6]) / 2);
+  cnt[9] = Math.abs(cnt[9] - cnt[6]);
+
+  const max = Math.max(...cnt);
+  console.log(max);
+}
