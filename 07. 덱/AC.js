10b35dc653b73e4b54d04de69205657220620e19
uraflower <82873315+uraflower@users.noreply.github.com>
2024-01-16 02:16:16 +0900

    [ PS ] : 5430

---
+++ 07. 덱/AC.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const [T, ...input] = fs
+  // .readFileSync('/dev/stdin')
+  .readFileSync('./input.txt')
+  .toString()
+  .trim()
+  .split('\n');
+
+const result = [];
+solution();
+
+function solution() {
+  for (let i = 0; i < Number(T); i++) {
+    const functions = input[3 * i];
+    const array = parse(input[3 * i + 2]);
+    executeTestCase(functions, array);
+  }
+  console.log(result.join('\n'));
+}
+
+function executeTestCase(functions, array) {
+  let isReversed = false;
+  let left = 0;
+  let right = array.length - 1;
+
+  for (let name of functions) {
+    if (name === 'R') {
+      isReversed = !isReversed;
+      continue;
+    }
+
+    // case of name === 'D'
+    if (left > right) return result.push('error');
+    else isReversed ? right-- : left++;
+  }
+
+  const sliced = array.slice(left, right + 1);
+  if (isReversed) sliced.reverse();
+  result.push(`[${sliced.join(',')}]`);
+}
+
+function parse(string) {
+  return string
+    .substring(1, string.length - 1)
+    .split(',')
+    .filter((num) => num !== '');
+}
