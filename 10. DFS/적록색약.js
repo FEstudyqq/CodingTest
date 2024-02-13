7efaacb8099a9f349c9a3758fc5fa78475ae4a3f
chestnut1044 <wbk1620@gmail.com>
2024-02-13 15:38:56 +0900

    [ PS ] : 10026

--- 10. DFS/적록색약.js
+++ 10. DFS/적록색약.js
@@ -39,3 +39,35 @@ for (let i = 0; i < n; i++) {
 }
 
 console.log(count);
+
+// // 깊이우선탐색 - 재귀
+// function dfs(x, y, map, target) {
+//   if (!target) return false;
+//   if (x < 0 || y < 0 || x >= n || y >= n) return false;
+//   if (map[x][y] === target) {
+//     map[x][y] = false;
+//     dfs(x + 1, y, map, target);
+//     dfs(x - 1, y, map, target);
+//     dfs(x, y + 1, map, target);
+//     dfs(x, y - 1, map, target);
+//     return true;
+//   }
+// }
+
+// let cnt = 0;
+// for (let i = 0; i < n; i++) {
+//   for (let j = 0; j < n; j++) {
+//     if (dfs(i, j, graph, graph[i][j])) cnt++;
+//   }
+// }
+
+// console.log(cnt);
+
+// cnt = 0;
+// for (let i = 0; i < n; i++) {
+//   for (let j = 0; j < n; j++) {
+//     if (dfs(i, j, graphBlind, graphBlind[i][j])) cnt++;
+//   }
+// }
+
+// console.log(cnt);
