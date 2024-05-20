7e4cb1b24945a78fb8d0e5cba2c67878b431f4cc
Jung Wook Park <hatchling3713@gmail.com>
2024-05-20 22:24:30 +0900

    [ PS ] : 15650

---
+++ 12. 백트래킹/N과 M (2).js
@@ -0,0 +1,43 @@
+/**
+ * @param {number} M 
+ * @param {number} N 
+ * @param {number[]} result 
+ * @param {number[]} temp 
+ */
+function backtrack(M, N, result, temp) {
+  if (M === 0) {
+    const value = [...temp].sort((a, b) => a - b).join(' ');
+
+    if (!result.includes(value)) {
+      result.push(value);
+    }
+
+    return;
+  }
+
+  for (let i = 1; i <= N; i++) {
+    if (!temp.includes(i)) {
+      temp.push(i);
+      backtrack(M - 1, N, result, temp);
+      temp.pop();
+    }
+  }
+}
+
+const fs = require('fs');
+
+const input = fs.readFileSync('/dev/stdin').toString().trim();
+const [N, M] = input.split(' ').map(str => Number.parseInt(str, 10));
+
+/**
+ * @type {number[]}
+ */
+const result = [];
+/**
+ * @type {number[]}
+ */
+const temp = [];
+
+backtrack(M, N, result, temp);
+
+console.log(result.join('\n'));
