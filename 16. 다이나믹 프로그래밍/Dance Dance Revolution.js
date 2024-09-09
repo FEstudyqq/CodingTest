6e1c00f139279fb1f62577fa572b38f963f3c1c0
devkyoung2 <devkyoung2@gmail.com>
2024-09-09 13:54:00 +0900

    [ PS ] : 2342

--- 16. 다이나믹 프로그래밍/Dance Dance Revolution.js
+++ 16. 다이나믹 프로그래밍/Dance Dance Revolution.js
@@ -9,23 +9,28 @@ const getEnergy = (a, b) => {
   else return 3;
 };
 
-const n = inputs.length;
+const n = inputs.length - 1;
 const dp = Array(n)
   .fill()
   .map(() =>
     Array(5)
       .fill()
-      .map(() => Array(5).fill(-1))
+      .map(() => Array(5).fill(false))
   );
 
 const dfs = (step, l, r) => {
-  if (step >= n - 1) return 0;
-  if (dp[step][l][r] !== -1) return dp[step][l][r];
+  // 재귀함수 종료 조건
+  if (step >= n) return 0;
+  // 이미 방문하여 메모제이션이 됐다면,
+  if (dp[step][l][r]) return dp[step][l][r];
 
+  // 각 발을 움직였을때의 값
   const moveLeft = dfs(step + 1, inputs[step], r) + getEnergy(l, inputs[step]);
   const moveRight = dfs(step + 1, l, inputs[step]) + getEnergy(r, inputs[step]);
 
+  // 두 값중 최소값으로 갱신
   dp[step][l][r] = Math.min(moveLeft, moveRight);
+
   return dp[step][l][r];
 };
 
