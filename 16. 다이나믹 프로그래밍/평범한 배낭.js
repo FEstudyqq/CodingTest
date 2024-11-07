8f32178d259dcde5de54ccfb4ebe2e82b2306360
devkyoung2 <devkyoung2@gmail.com>
2024-11-07 13:52:39 +0900

    [ PS ] : 12865

---
+++ 16. 다이나믹 프로그래밍/평범한 배낭.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+const [[n, k], ...inputs] = fs
+  .readFileSync(path)
+  .toString()
+  .trim()
+  .split('\n')
+  .map((line) => line.split(' ').map(Number));
+const dp = Array(k + 1).fill(0);
+
+for (let i = 0; i < n; i++) {
+  const [w, v] = inputs[i];
+
+  for (let j = k + 1; j >= w; j--) {
+    dp[j] = Math.max(dp[j], dp[j - w] + v);
+  }
+}
+
+console.log(dp[k]);
