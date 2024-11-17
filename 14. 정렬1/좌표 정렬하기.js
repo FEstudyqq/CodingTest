26bc1e24aedc96d2a2b4cf624631b185b3b5821d
devkyoung2 <devkyoung2@gmail.com>
2024-11-17 14:54:07 +0900

    [ PS ] : 11650

---
+++ 14. 정렬1/좌표 정렬하기.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const path = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+const [[n], ...inputs] = fs
+  .readFileSync(path)
+  .toString()
+  .trim()
+  .split('\n')
+  .map((it) => it.split(' ').map(Number));
+const ans = inputs
+  .sort((a, b) => a[0] - b[0] || a[1] - b[1])
+  .map(([x, y]) => `${x} ${y}`)
+  .join('\n');
+console.log(ans);
