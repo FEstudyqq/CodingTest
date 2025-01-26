04a487fec88ea9f2e9258540ee608d860947b39e
uraflower <82873315+uraflower@users.noreply.github.com>
2025-01-27 00:36:37 +0900

    [ PS ] : 15652

---
+++ 12. 백트래킹/N과 M (4).js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const [N, M] = fs
+  // .readFileSync('/dev/stdin')
+  .readFileSync('../input.txt')
+  .toString()
+  .trim()
+  .split(' ')
+  .map(Number);
+
+const answer = [];
+
+function recurse(start, sequence) {
+  for (let i = start; i <= N; i++) {
+    sequence.push(i);
+
+    if (sequence.length === M) {
+      answer.push(sequence.join(' '));
+    } else {
+      recurse(i, sequence);
+    }
+
+    sequence.pop();
+  }
+}
+recurse(1, []);
+console.log(answer.join('\n'));
