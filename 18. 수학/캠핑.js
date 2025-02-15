b30cfc5ec1ed3604f82fa9df75d13e0dcf79fdfd
uraflower <uraflower@ajou.ac.kr>
2025-02-15 10:58:59 +0900

    [ PS ] : 4796.js

---
+++ 18. 수학/캠핑.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const cases = fs
+  // .readFileSync('/dev/stdin')
+  .readFileSync('../input.txt')
+  .toString()
+  .trim()
+  .split('\n')
+  .map((line) => line.split(' ').map(Number));
+
+cases.pop();
+
+const answers = cases.map(([l, p, v], idx) => {
+  let m = l * Math.floor(v / p);
+  let n = v % p <= l ? v % p : l;
+
+  return `Case ${idx + 1}: ${m + n}`;
+});
+
+console.log(answers.join('\n'));
